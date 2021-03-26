57c6892233188728162607fc95895b15e5e2a983
Nakai <nakai.hamlin18@gmail.com>
2021-03-26 01:50:57 -0400

    commit tree 2876b97ed5d6bb914f27baa3c54445695ff6e3af

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 19, 'M', 'Brown', 'Relaxed', 1),
+('Rowdy', 18, 'F', 'Black', 'Wild', 1),
+('Tabitha', 17, 'F', 'Black', 'Crazy', 1),
+('Sargeant Brown', 20, 'M', 'Panda', 'Bold', 0),
+('Melissa', 17, 'F', 'Polar', 'Calm', 1),
+('Grinch', 21, 'M', 'Kodiak', 'Lonely', 0),
+('Wendy', 16, 'F', 'American', 'Modest', 0),
+(NULL, 17, 'F', 'Sloth', 'Sassy', 1);
